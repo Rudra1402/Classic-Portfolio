c5371ba36cc398033fb1fa6dfe19773107836053
Rudra Patel <rudra.190410116095@gmail.com>
2024-08-27 23:27:24 -0400

    updated google maps embed link

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,7 @@ function Contact() {
                     </div>
                     <div className="border-t border-gray-300 rounded-xl">
                         <iframe
-                            title="Google Maps"
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12623.073723328213!2d-83.04925118862186!3d42.31743223201452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b65d90f5f9b19%3A0x1ad7b0d118affbc3!2sWindsor%2C%20ON%2C%20Canada!5e0!3m2!1sen!2sus!4v1594666563580!5m2!1sen!2sus"
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5901.582625522756!2d-83.06861392543153!3d42.30431813862373!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x883b2d77c6ec4bef%3A0x1a44f1192a4e41ee!2sUniversity%20of%20Windsor!5e0!3m2!1sen!2sca!4v1724815505586!5m2!1sen!2scas"
                             width="100%"
                             height="300"
                             frameBorder="0"
